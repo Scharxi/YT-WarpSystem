92dc13016cbce57df0276d3440307093d69bb142
BufferOverflow <scharxidev@outlook.de>
2022-10-03 21:39:10 +0200

    code style adjustment: replaced nested if statements

--- src/main/java/me/betagameryoutube/warpsystem/commands/SetWarpCommand.java
+++ src/main/java/me/betagameryoutube/warpsystem/commands/SetWarpCommand.java
@@ -12,45 +12,49 @@ import java.io.File;
 import java.io.IOException;
 import java.util.regex.Pattern;
 
-public class SetWarpCommand implements CommandExecutor {
+public final class SetWarpCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if(sender instanceof Player) {
-            Player p = (Player) sender;
-            if(p.hasPermission("warps.set")) {
-                if(args.length == 1) {
-                    if(Pattern.matches("[a-zA-Z]+", args[0].toLowerCase())) {
-                        File file = new File("./plugins/Warps/warps/" + args[0].toLowerCase() + ".yml");
-                        if(!file.exists()) {
-                            YamlConfiguration yfile = YamlConfiguration.loadConfiguration(file);
-                            Location loc = p.getLocation();
-                            yfile.set("World", loc.getWorld().getName());
-                            yfile.set("X", loc.getX());
-                            yfile.set("Y", loc.getY());
-                            yfile.set("Z", loc.getZ());
-                            yfile.set("Yaw", loc.getYaw());
-                            yfile.set("Pitch", loc.getPitch());
-                            try {
-                                yfile.save(file);
-                            } catch (IOException exception) {
-                                exception.printStackTrace();
-                            }
-                            p.sendMessage(Config.getPrefix() + "§aWarp set!");
-                        } else {
-                            p.sendMessage(Config.getPrefix() + "§cThis Warp already exist!");
-                        }
-                    } else {
-                        p.sendMessage(Config.getPrefix() + "§cWarpname can only contain letters!");
-                    }
-                } else {
-                    p.sendMessage(Config.getPrefix() + "§7Usage: §c/setwarp <Warpname>");
-                }
-            } else {
-                p.sendMessage(Config.getPrefix() + "§cNo Permission!");
-            }
-        } else {
+        if (!(sender instanceof Player)) {
             sender.sendMessage(Config.getPrefix() + "§cYou must be a Player!");
+            return true;
+        }
+
+        Player p = (Player) sender;
+        if (!p.hasPermission("warps.set")) {
+            p.sendMessage(Config.getPrefix() + "§cNo Permission!");
+            return true;
+        }
+
+        if (args.length != 1) {
+            p.sendMessage(Config.getPrefix() + "§7Usage: §c/setwarp <Warpname>");
+            return true;
+        }
+
+        if (!Pattern.matches("[a-zA-Z]+", args[0].toLowerCase())) {
+            p.sendMessage(Config.getPrefix() + "§cWarpname can only contain letters!");
+            return true;
+        }
+
+        File file = new File("./plugins/Warps/warps/" + args[0].toLowerCase() + ".yml");
+        if (!file.exists()) {
+            YamlConfiguration yfile = YamlConfiguration.loadConfiguration(file);
+            Location loc = p.getLocation();
+            yfile.set("World", loc.getWorld().getName());
+            yfile.set("X", loc.getX());
+            yfile.set("Y", loc.getY());
+            yfile.set("Z", loc.getZ());
+            yfile.set("Yaw", loc.getYaw());
+            yfile.set("Pitch", loc.getPitch());
+            try {
+                yfile.save(file);
+            } catch (IOException exception) {
+                exception.printStackTrace();
+            }
+            p.sendMessage(Config.getPrefix() + "§aWarp set!");
+        } else {
+            p.sendMessage(Config.getPrefix() + "§cThis Warp already exist!");
         }
         return false;
     }
