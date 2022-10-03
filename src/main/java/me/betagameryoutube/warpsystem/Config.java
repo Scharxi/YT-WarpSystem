23bf01c0b08750e3cbe5cff54bd5cc979c09a6cb
BufferOverflow <scharxidev@outlook.de>
2022-10-03 21:32:56 +0200

    made config class final

--- src/main/java/me/betagameryoutube/warpsystem/Config.java
+++ src/main/java/me/betagameryoutube/warpsystem/Config.java
@@ -4,7 +4,7 @@ import org.bukkit.configuration.file.YamlConfiguration;
 
 import java.io.File;
 
-public class Config {
+public final class Config {
     public static String getPrefix() {
         File file = new File("./plugins/Warps/config.yml");
         YamlConfiguration yfile = YamlConfiguration.loadConfiguration(file);
