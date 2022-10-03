1533f26802007886fa9f11570bea8d9339d12db7
BufferOverflow <scharxidev@outlook.de>
2022-10-03 21:57:23 +0200

    code style adjustments: replaced nested if statements

--- src/main/java/me/betagameryoutube/warpsystem/commands/WarpsCommand.java
+++ src/main/java/me/betagameryoutube/warpsystem/commands/WarpsCommand.java
@@ -14,56 +14,57 @@ public class WarpsCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (sender instanceof Player) {
-            Player p = (Player) sender;
-            if (p.hasPermission("warps.list")) {
-                if (args.length == 0) {
-                    File xdir = new File("./plugins/Warps/warps");
-                    if (!xdir.exists()) {
-                        xdir.mkdirs();
-                    }
-                    List<String> warplist = new ArrayList<>();
-                    File[] dir = new File("./plugins/Warps/warps").listFiles();
-                    for (File file : dir) {
-                        if (file.isFile()) {
-                            warplist.add(file.getName().replace(".yml", ""));
-                        }
-                    }
-                    String warps = warplist.toString().replace("[", "").replace("]", "");
-                    if (warplist.isEmpty()) {
-                        p.sendMessage(Config.getPrefix() + "§bThere are no Warps!");
-                    } else {
-                        p.sendMessage(Config.getPrefix() + "§bWarps: §e" + warps);
-                    }
-                } else {
-                    p.sendMessage(Config.getPrefix() + "§7Usage: §c/warps");
-                }
-            } else {
-                p.sendMessage(Config.getPrefix() + "§cNo Permission!");
+
+        if (args.length > 0) {
+            sender.sendMessage(Config.getPrefix() + "§7Usage: §c/warps");
+            return true;
+        }
+
+        if (!(sender instanceof Player)) {
+            File xdir = new File("./plugins/Warps/warps");
+            if (!xdir.exists()) {
+                xdir.mkdirs();
             }
-        } else {
-            if (args.length == 0) {
-                File xdir = new File("./plugins/Warps/warps");
-                if (!xdir.exists()) {
-                    xdir.mkdirs();
+            List<String> warpList = new ArrayList<>();
+            File[] dir = new File("./plugins/Warps/warps").listFiles();
+            for (File file : dir) {
+                if (file.isFile()) {
+                    warpList.add(file.getName().replace(".yml", ""));
                 }
-                List<String> warplist = new ArrayList<>();
-                File[] dir = new File("./plugins/Warps/warps").listFiles();
-                for (File file : dir) {
-                    if (file.isFile()) {
-                        warplist.add(file.getName().replace(".yml", ""));
-                    }
-                }
-                String warps = warplist.toString().replace("[", "").replace("]", "");
-                if (warplist.isEmpty()) {
-                    sender.sendMessage(Config.getPrefix() + "§bThere are no Warps!");
-                } else {
-                    sender.sendMessage(Config.getPrefix() + "§bWarps: §e" + warps);
-                }
-            } else {
-                sender.sendMessage(Config.getPrefix() + "§7Usage: §c/warps");
+            }
+            String warps = warpList.toString().replace("[", "").replace("]", "");
+            if (warpList.isEmpty()) {
+                sender.sendMessage(Config.getPrefix() + "§bThere are no Warps!");
+                return true;
+            }
+
+            sender.sendMessage(Config.getPrefix() + "§bWarps: §e" + warps);
+            return true;
+        }
+
+        Player p = (Player) sender;
+        if (!p.hasPermission("warps.list")) {
+            p.sendMessage(Config.getPrefix() + "§cNo Permission!");
+            return true;
+        }
+
+        File xdir = new File("./plugins/Warps/warps");
+        if (!xdir.exists()) {
+            xdir.mkdirs();
+        }
+        List<String> warpList = new ArrayList<>();
+        File[] dir = new File("./plugins/Warps/warps").listFiles();
+        for (File file : dir) {
+            if (file.isFile()) {
+                warpList.add(file.getName().replace(".yml", ""));
             }
         }
+        String warps = warpList.toString().replace("[", "").replace("]", "");
+        if (warpList.isEmpty()) {
+            p.sendMessage(Config.getPrefix() + "§bThere are no Warps!");
+            return true;
+        }
+        p.sendMessage(Config.getPrefix() + "§bWarps: §e" + warps);
         return false;
     }
 }
